61b05e24b6ac654b8ade115748800bf00903a0a4
Mike Cao <moocao@gmail.com>
2024-07-18 22:30:34 -0700

    Added GitHub pages workflow.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
+  basePath: '/react-zen',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = withMDX(nextConfig);
