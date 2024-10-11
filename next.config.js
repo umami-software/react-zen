f348318ce3f729dba8baef76139e69b729b4735c
Mike Cao <moocao@gmail.com>
2024-10-11 15:13:28 -0700

    Updated default build to esbuild.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = withMDX(nextConfig);
