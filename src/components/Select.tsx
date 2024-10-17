0749fe16bd2ac97c5ea98b2d91df0a725b63c0d4
Mike Cao <mike@mikecao.com>
2024-10-17 16:48:29 -0700

    Updated select component.

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,39 +1,44 @@
 import { ReactNode } from 'react';
 import classNames from 'classnames';
-import { Button, Select as AriaSelect, SelectValue } from 'react-aria-components';
+import {
+  Button,
+  Select as AriaSelect,
+  SelectProps as AriaSelectProps,
+  SelectValue,
+  Label,
+} from 'react-aria-components';
 import { List } from './List';
 import { ListItem } from './ListItem';
 import { Popover } from './Popover';
 import { Icon } from './Icon';
 import { Icons } from './Icons';
+import { Row } from './Row';
+import inputStyles from './Input.module.css';
 import styles from './Select.module.css';
 
-export interface SelectProps {
+interface SelectProps extends AriaSelectProps<HTMLSelectElement> {
   items: any[];
   value: string;
-  className?: string;
-  children?: ReactNode;
+  label?: string;
 }
 
-function Select({ children, items = [], value, className, ...props }: SelectProps) {
+function Select({ children, items = [], value, label, className, ...props }: SelectProps) {
   return (
-    <AriaSelect {...props} className={classNames(styles.select, className)}>
-      <Button className={styles.button}>
-        <SelectValue />
-        <span aria-hidden="true">
-          <Icon rotate={90} size="xs" className={styles.icon}>
-            <Icons.Chevron />
-          </Icon>
-        </span>
+    <AriaSelect {...props} className={classNames(inputStyles.field, className)}>
+      {label && <Label className={inputStyles.label}>{label}</Label>}
+      <Button className={classNames(inputStyles.input, className)}>
+        <Row justifyContent="space-between" gap="md">
+          <SelectValue />
+          <span aria-hidden="true">
+            <Icon rotate={90} size="xs" className={inputStyles.icon}>
+              <Icons.Chevron />
+            </Icon>
+          </span>
+        </Row>
       </Button>
       <Popover>
         <List items={items} className={styles.list}>
-          {children ||
-            items.map(item => (
-              <ListItem key={item} id={item} className={styles.item}>
-                {item}
-              </ListItem>
-            ))}
+          {children as any}
         </List>
       </Popover>
     </AriaSelect>
@@ -41,3 +46,4 @@ function Select({ children, items = [], value, className, ...props }: SelectProp
 }
 
 export { Select };
+export type { SelectProps };
