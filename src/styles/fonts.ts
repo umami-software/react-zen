0eaae325af9ee98abba25576c7b68639e27ef1a0
Mike Cao <moocao@gmail.com>
2024-12-02 16:48:16 -0800

    Updated fonts.

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,6 @@
 import { Inter, JetBrains_Mono } from 'next/font/google';
 
-const inter = Inter();
-const jetBrainsMono = JetBrains_Mono();
+const inter = Inter({ subsets: ['latin'] });
+const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] });
 
 export { inter, jetBrainsMono };
