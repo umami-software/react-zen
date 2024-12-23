65ec36e5514064fdacef779cb41811cdf8eee6db
Mike Cao <mike@mikecao.com>
2024-12-23 11:40:36 -0800

    Added static generation.

--- src/app/docs/[[...id]]/page.tsx
+++ src/app/docs/[[...id]]/page.tsx
@@ -1,6 +1,14 @@
-import { getContent } from '@umami/shiso/server';
+import { getContent, getContentIds } from '@umami/shiso/server';
 import config from '@/shiso.config.json';
 import { DocsPage } from './DocsPage';
+
+export async function generateStaticParams() {
+  const ids = await getContentIds('./src/content/docs');
+
+  return ids.map((id: string) => ({
+    id: id.split('/'),
+  }));
+}
 
 export default async function ({ params }: { params: Promise<{ id: string[] }> }) {
   const content = await getContent(await params, './src/content/docs');
