89c9df695225e4e188c210c9363029b6fb16af78
Mike Cao <mike@mikecao.com>
2024-12-29 23:05:09 -0800

    Updated Shiso.

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,11 @@
+import config from '@/shiso.config.json';
+import { next } from '@umami/shiso/server';
+import { DocsPage } from './DocsPage';
+
+const { generateMetadata, generateStaticParams, renderPage } = next('docs', config);
+
+export { generateMetadata, generateStaticParams };
+
+export default renderPage(props => {
+  return <DocsPage {...props} />;
+});
