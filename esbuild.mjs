a957235b79b45838c3e16ff19a508cf31ae5d7b9
Mike Cao <moocao@gmail.com>
2025-02-14 20:32:06 -0800

    Downgrade RAC because tooltip stopped working.

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,14 +1,41 @@
 import esbuild from 'esbuild';
 import { commonjs } from '@hyrious/esbuild-plugin-commonjs';
 import cssModules from '@umami/esbuild-plugin-css-modules';
-import svgr from 'esbuild-plugin-svgr'
+import svgr from 'esbuild-plugin-svgr';
+
+const nullImportPlugin = ({ matches = [] }) => ({
+  name: 'null-import',
+  setup(build) {
+    // Handle imports during resolution
+    build.onResolve({ filter: /.*/ }, args => {
+      if (matches.some(match => match.test(args.path))) {
+        return {
+          path: args.path,
+          namespace: 'null-import',
+        };
+      }
+      return null;
+    });
+
+    // Replace matched imports with undefined
+    build.onLoad({ filter: /.*/, namespace: 'null-import' }, () => ({
+      contents: `export default undefined;`,
+      loader: 'js',
+    }));
+  },
+});
 
 const config = {
   entryPoints: ['src/components/index.ts'],
   bundle: true,
   external: ['react', 'react-dom', 'react-jsx/runtime'],
   jsx: 'automatic',
-  plugins: [commonjs(), cssModules(), svgr()],
+  plugins: [
+    commonjs(),
+    cssModules(),
+    svgr(),
+    nullImportPlugin({ matches: [/[a-z]-[A-Z]/] }),
+  ],
 };
 
 esbuild
