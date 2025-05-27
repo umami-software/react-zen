bcd2bc0cad838c7485ffb215860440bbc92c280b
Mike Cao <mike@mikecao.com>
2025-05-26 23:04:42 -0700

    Refactored text field.

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Row, Text, ThemeButton, Button, Icon } from '@/components';
-import Github from '@/assets/github.svg';
+import { Github } from '@/components/icons';
 
 export function Header() {
   return (
