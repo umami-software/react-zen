08b2d9369d964f15cad88ee270cc8702eec5e88c
Mike Cao <moocao@gmail.com>
2025-06-04 16:37:27 -0700

    Added label, description and error props to FormFieldArray.

--- src/components/forms/FormFieldArray.tsx
+++ src/components/forms/FormFieldArray.tsx
@@ -1,18 +1,40 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
 import { useFieldArray, useFormContext } from 'react-hook-form';
+import classNames from 'classnames';
+import { Label } from '@/components';
+import styles from './FormField.module.css';
 
-export interface FormFieldArrayProps {
+export interface FormFieldArrayProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
   name: string;
+  description?: string;
+  label?: string;
   children: (props: any) => ReactNode;
 }
 
-export function FormFieldArray({ name, children }: FormFieldArrayProps) {
+export function FormFieldArray({
+  name,
+  description,
+  label,
+  className,
+  children,
+  ...props
+}: FormFieldArrayProps) {
   const context = useFormContext();
-  const { control } = context;
-  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+  const { control, formState } = context;
+  const fieldProps = useFieldArray({
     control,
     name,
   });
 
-  return children({ context, fields, append, prepend, remove, swap, move, insert });
+  const errors = formState?.errors || {};
+  const errorMessage = errors[name]?.message as string;
+
+  return (
+    <div {...props} className={classNames(styles.input, className)}>
+      {label && <Label>{label}</Label>}
+      {description && <div className={styles.description}>{description}</div>}
+      {errorMessage && <div className={styles.error}>{errorMessage}</div>}
+      {children({ context, ...fieldProps })}
+    </div>
+  );
 }
