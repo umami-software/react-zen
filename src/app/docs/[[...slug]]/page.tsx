a624cf2fdeff6b62ca2fecae83d286fc3f2232a3
AbhiVarde <abhivarde99@gmail.com>
2025-11-26 14:45:13 +0530

    fix: resolve 404 error on /docs route navigation

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,11 +1,16 @@
-import { next } from '@umami/shiso/server';
-import config from '@/app/shiso.config.json';
-import { DocsPage } from './DocsPage';
+import { next } from "@umami/shiso/server";
+import config from "@/app/shiso.config.json";
+import { DocsPage } from "./DocsPage";
 
-const { generateMetadata, generateStaticParams, renderPage } = next('docs', config);
+const { generateMetadata, generateStaticParams, renderPage } = next(
+  "docs",
+  config
+);
 
 export { generateMetadata, generateStaticParams };
 
-export default renderPage(props => {
+export const dynamicParams = true;
+
+export default renderPage((props) => {
   return <DocsPage {...props} />;
 });
