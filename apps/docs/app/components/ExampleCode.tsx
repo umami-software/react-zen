dd29a05fc2f3aca4f80081139c6ee5e70da3c109
Mike Cao <mike@mikecao.com>
2026-07-26 21:37:56 -0700

    Style code block copy button and center controls in single-line snippets

--- apps/docs/app/components/ExampleCode.tsx
+++ apps/docs/app/components/ExampleCode.tsx
@@ -12,7 +12,7 @@ export function ExampleCode({ children }: ExampleCodeProps) {
       position="relative"
       overflow="auto"
       backgroundColor="surface-base"
-      className="[&>*]:!m-0 [&_*]:border-0 [&_*]:ring-0 [&_pre]:!m-0 [&_pre]:p-4 [&_pre]:text-sm [&_figure]:!m-0 [&_figure]:!p-0 [&_div]:!m-0 [&_code]:font-[family-name:var(--font-family-mono)]"
+      className="example-code [&>*]:!m-0 [&_*]:border-0 [&_*]:ring-0 [&_pre]:!m-0 [&_pre]:p-4 [&_pre]:text-sm [&_figure]:!m-0 [&_figure]:!p-0 [&_div]:!m-0 [&_code]:font-[family-name:var(--font-family-mono)]"
     >
       {children}
     </Box>
